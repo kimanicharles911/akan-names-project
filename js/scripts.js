887c9eb0177f42113ecc867c50e694cec81fabaa
kimanicharles911 <kimanicharles911@gmail.com>
2020-02-14 14:16:13 +0300

    change name attribute type for number input types in html & css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function myFunction (form){
-  var day_byte = form.d_input1.value;
-  var month_byte = form.d_input2.value;
-  var century_byte = form.d_input3.value;
-  var year_digit_byte = form.d_input4.value
+  var day_byte = form.d_input.value;
+  var month_byte = form.m_input.value;
+  var century_byte = form.c_input.value;
+  var year_digit_byte = form.yd_input.value
   var gender_byte = form.gender.value;
   console.log("Day: " + day_byte + ", Month: " + month_byte + ", Century: " + century_byte +  ", Year digit: " + year_digit_byte + " and " + gender_byte);
   //document.getElementById("form_1").submit();
