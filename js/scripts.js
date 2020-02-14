cbba9d42f846ff6febed6b7f685b5083e779851a
kimanicharles911 <kimanicharles911@gmail.com>
2020-02-14 12:39:51 +0300

    store date and gender in variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-function myFuntion (){
-  document.getElementById("form-1").submit();
+function myFunction (form){
+  var date_byte = form.form_b.value;
+  var gender_byte = form.gender.value;
+  alert("You entered: " + date_byte + " and " + gender_byte);
+  //document.getElementById("form_1").submit();
 }
+
+
+
+
+
+
+
+
+
+
+
+/*function testResults (form) {
+    var TestVar = form.inputbox.value;
+    alert ("You typed: " + TestVar);
+}*/
