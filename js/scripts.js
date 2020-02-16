66fc2baf39f6a789efa893fc1fdea11bc3a01317
kimanicharles911 <kimanicharles911@gmail.com>
2020-02-16 23:48:31 +0300

    apply dayOfWeek formula

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ function myFunction2() {
   let day_byte = parseInt(date_byte[2]);
 
   let century_byte = date_byte[0].substr(0, 2);
+  let formulae = ( ((century_byte/4) -2*century_byte-1) + ((5*year_byte/4)) + ((26*(month_byte+1)/10))+day_byte ) % 7;
 
   console.log("The Date is " + date_input);
   console.log("The Year input: " + year_byte);
@@ -16,5 +17,6 @@ function myFunction2() {
   console.log("The month input: " + month_byte);
 
   console.log("The Century extracted: " + century_byte);
+  console.log("Day you were born:" + formulae);
 
 }
