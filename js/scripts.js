570856f9ecc4107e0c1e4356a73a67caf427989a
kimanicharles911 <kimanicharles911@gmail.com>
2020-02-16 23:40:09 +0300

    add form reset function to be called later

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-function myFunction (form){
-  var day_byte = form.d_input.value;
-  var month_byte = form.m_input.value;
-  var century_byte = form.c_input.value;
-  var year_digit_byte = form.yd_input.value
-  var gender_byte = form.gender.value;
-  console.log("Day: " + day_byte + ", Month: " + month_byte + ", Century: " + century_byte +  ", Year digit: " + year_digit_byte + " and " + gender_byte);
-  //document.getElementById("form_1").submit();
+function functionNewForm() {
+  document.getElementById("form1").reset();
 }
-
-
-
-
-
-
-
-
-
-
-
-/*function testResults (form) {
-    var TestVar = form.inputbox.value;
-    alert ("You typed: " + TestVar);
-}*/
