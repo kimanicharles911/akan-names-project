b120d36e32e59f63870962d10da776dad714eca5
kimanicharles911 <kimanicharles911@gmail.com>
2020-02-16 23:45:30 +0300

    get year, day, month with array

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,14 @@ function functionNewForm() {
 function myFunction2() {
   let date_input = document.getElementById("date_input").value;
   let date_byte = date_input.split("-");
+  let year_byte = parseInt(date_byte[0]);
+  let month_byte = parseInt(date_byte[1]);
+  let day_byte = parseInt(date_byte[2]);
+
+
+  console.log("The Date is " + date_input);
+  console.log("The Year input: " + year_byte);
+  console.log("The Day input: " + day_byte);
+  console.log("The month input: " + month_byte)
+
 }
