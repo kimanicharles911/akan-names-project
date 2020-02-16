710b9673eff55a5f54edcdb7d3f26dc8e4ce2b7b
kimanicharles911 <kimanicharles911@gmail.com>
2020-02-17 00:01:23 +0300

    add space in between funtions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function functionNewForm() {
   document.getElementById("form1").reset();
 }
+
 function myFunction2() {
   let date_input = document.getElementById("date_input").value;
   let date_byte = date_input.split("-");
